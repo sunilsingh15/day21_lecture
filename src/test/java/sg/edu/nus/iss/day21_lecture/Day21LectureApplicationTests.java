5276b833c703b1a2120e03e4d97f2c895809afca
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-20 12:26:52 +0800

    initial commit

---
+++ src/test/java/sg/edu/nus/iss/day21_lecture/Day21LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day21_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
